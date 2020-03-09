8e8db7d2cb9b415bd80b5d0084408c2501b1be17
NadiMarshmallow <45483023+NadiMarshmallow@users.noreply.github.com>
2020-03-09 19:10:59 +0100

    Add files via upload

---
+++ to do list/js/todolist.js
@@ -0,0 +1,18 @@
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Scrivi cosa dovresti fare");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u002C");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+}
